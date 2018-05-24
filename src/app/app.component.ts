04ee2e9da0895501395dffeea4917a739ad868c9
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2018-05-24 15:44:28 -0300

    Loading added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Request } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  private baseUrl = 'https://swapi.co/api/';
+
+  people: any = [];
+  count: number = 0;
+  next: string;
+  previous: string;
+  searchInput: string;
+  isLoading: boolean = true;
+
+  constructor(private http: Http) {
+    this.getPeople(this.baseUrl+'people');
+  }
+
+  getPeople(url) {
+    this.isLoading = true;
+    this.http.get(url)
+      .subscribe(data => {
+      var body = data.json();
+      this.people = body.results;
+      this.next = body.next;
+      this.previous = body.previous;
+      this.count = body.count;
+      this.isLoading = false;
+    });
+  }
+
+  getPreviousPage() {
+    this.getPeople(this.previous);
+  }
+
+  getNextPage() {
+    this.getPeople(this.next);
+  }
+
+  searchPeople() {
+    this.getPeople(this.baseUrl + 'people/?search=' + this.searchInput);
+  }
 }
